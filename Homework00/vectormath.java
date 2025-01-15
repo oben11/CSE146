676973a982c9db62da93ed99e69692a1b1218879
oben11 <dithskii@gmail.com>
2025-01-14 22:38:29 -0500

    first commit?

--- Homework00/vectormath.java
+++ Homework00/vectormath.java
@@ -1,6 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * git add .
+ * git add specific.file
+ * git commit -m "commit message"
+ * git push
  */
 
 /**
@@ -8,6 +12,6 @@
  * @author obenjamin
  */
 public class vectormath {
-    system.out.println("hello world");
+    system.out.println("hello world anything else");
 
 }
